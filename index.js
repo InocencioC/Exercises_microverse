84904a13ccdf873d499ae57e0ff766a0c3c5e611
Inocêncio Cordeiro <>
2022-09-24 09:15:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let username = "Inocêncio";
+
+console.log("Hello", username, "How are you ?");
+
+let totalapples = 20;
+let totalusers = 5;
+
+let applesperuser = totalapples / totalusers;
+
+console.log("You have", applesperuser, "apples");
