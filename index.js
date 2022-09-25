4e48a512b23af46fcb888aec0793bf5a863e7edf
Inocencio Cordeiro <34503843+InocencioC@users.noreply.github.com>
2022-09-25 13:23:22 +0000

    exercise 12 conditions

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-let A = 4 + 7;
-let B = 5 * 9;
-let C = B / A;
+let X = 32;
 
-console.log(C)
+if ( X == 9) {
+  console.log("True");
+}
+else {
+   console.log("False");
+}
+
+////////////////////////////////
+let Y = "Blue";
+
+if ( Y == "Blue") {
+  console.log("Blue detected");
+}
+else if (Y == "Green" ) {
+  console.log("Green detected");
+} else  {
+console.log("No green or blue detected");
+  
+}
