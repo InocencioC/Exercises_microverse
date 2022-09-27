cccf304ad13efa1bdb977614f75026681e291dd3
Inocencio Cordeiro <34503843+InocencioC@users.noreply.github.com>
2022-09-27 17:30:30 +0000

    method includes

--- ExerciseIncludes.js
+++ ExerciseIncludes.js
@@ -1,18 +1,11 @@
-let users = ["hackerman", "zerocool", "mitnick"];
+let users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let login = "oscar";
+let found = 13;
 
-if (users.includes(login) === true) {
-  console.log("Welcome");
+if (users.includes(found) === true) {
+  console.log("Found");
 } else {
-  console.log("Begone hacker");
-}
-/// FIND THE NUMBER 7 IN ARRAY
-let arr = [9, 3, 9, 4, 8];
-let find = 7;
-
-if (arr.includes(find) === true) {
-  console.log("It is included");
-} else {
-  console.log("it's just not there");
+  console.log("Not found");
+  users.push(13)
+  console.log(users);
 }
