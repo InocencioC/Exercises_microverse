53210db1458f7f8cf420ad66a5e7248e32dcb515
Inocencio Cordeiro <34503843+InocencioC@users.noreply.github.com>
2022-09-27 17:00:40 +0000

    Value in array and INCLUDES

---
+++ ValueInArray.js
@@ -0,0 +1,18 @@
+let users = ["hackerman", "zerocool", "mitnick"];
+
+let login = "oscar";
+
+if (users.includes(login) === true) {
+  console.log("Welcome");
+} else {
+  console.log("Begone hacker");
+}
+/// FIND THE NUMBER 7 IN ARRAY
+let arr = [9, 3, 9, 4, 8];
+let find = 7;
+
+if (arr.includes(find) === true) {
+  console.log("It is included");
+} else {
+  console.log("it's just not there");
+}
