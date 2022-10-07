1f1a75e0525c3ed6a1b528348ee478afef206155
Inocencio Cordeiro <34503843+InocencioC@users.noreply.github.com>
2022-10-07 14:01:26 +0100

    New version
    This is a new version without bug

---
+++ NumberParCorrect.java
@@ -0,0 +1,21 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar:
+
+public class App {
+    public static void main(String[] args){
+
+// TODO: Crie um Array de números inteiros:
+     int par;
+     
+     int[] array = { 2, 3, 5, 7, 11, 13, 18, 34 };
+
+// TODO: Implemente um laço de repetição que verifique APENAS seus números Pares:
+     for (par = 0; par < array.length; par++){
+         if (array[par] % 2 == 0){
+        
+                 System.out.println(array[par]);
+                } 
+            }
+           
+    }
+
+}
